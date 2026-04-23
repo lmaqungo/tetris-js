5a610e46330d147190c5b8147e8fb6cfc23650db
luya <91703950+lmaqungo@users.noreply.github.com>
2026-04-23 17:30:01 +0200

    feat: detach horizontal input event handling
    Moved event handling to separate method, called by
    the draw method.
    Created a method that generates a new tetromino
    instance. Right now only creates the same shape
    with a randomised colour.
    shapeContainer is now an object property to allow
    state management.

--- src/classes/userInterface.js
+++ src/classes/userInterface.js
@@ -2,16 +2,18 @@ import OmegaTetromino from "./tetromino/omegaTetromino.js";
 import IotaTetromino from "./tetromino/iotaTetromino.js";
 
 class UI {
-    cellSize;
-    rows;
-    columns;
-    grid; 
-    horizontalControl = 0;
+    cellSize;  
+    rows;  
+    columns;  
+    grid;  
+    horizontalMove = 0;  
+    shapeContainer;  
+    fallingTetro= false;
     
     constructor(cellSize=24, columns=10, rows=20) {
         this.cellSize = cellSize; 
         this.rows = rows; 
-        this.columns = columns;
+        this.columns = columns; 
         const body = document.querySelector('body'); 
         body.classList.add('flex', 'justify-center', 'items-center', 'h-screen', 'gap-1'); 
         
@@ -26,23 +28,24 @@ class UI {
         this.grid.style.position = "relative"
         
         body.appendChild(this.grid)
-        const testOmega = new OmegaTetromino(4); 
-        const testIota = new IotaTetromino(1); 
-        this.drawTetromino(testIota); 
-        
     } 
 
-    drawTetromino(tetromino){
-        const shapeContainer = document.createElement('div'); 
+    drawTetromino() {
+        this.fallingTetro = true;
+        const tetromino = this.generateTetro()
+
+        this.horizontalMove = 0; 
+        this.shapeContainer = document.createElement('div'); 
         
-        shapeContainer.style.display = "grid"; 
-        shapeContainer.style.gridTemplateColumns = `repeat(${tetromino.shape.length}, ${this.cellSize}px)`; 
-        shapeContainer.style.gridTemplateRows = `repeat(${tetromino.shape.length}, ${this.cellSize}px)`; 
-        shapeContainer.style.position = "absolute"; 
+        this.shapeContainer.style.display = "grid"; 
+        this.shapeContainer.style.gridTemplateColumns = `repeat(${tetromino.shape.length}, ${this.cellSize}px)`; 
+        this.shapeContainer.style.gridTemplateRows = `repeat(${tetromino.shape.length}, ${this.cellSize}px)`; 
+        this.shapeContainer.style.position = "absolute"; 
+        this.shapeContainer.style.left = `0px`;
 
         const endAnimationPosition = (this.rows - (tetromino.shape.length));
 
-        console.log(`end vertical position: ${endAnimationPosition}`)
+        console.log(`end vertical position: ${endAnimationPosition}`);
 
         /* 
             this 2 will represent the number of cells below the falling piece in a particular column
@@ -50,7 +53,6 @@ class UI {
             in the event listener. The fall animation fn will need to accept a new endPosition upon every
             input event
         */
-
 
         /* 
         This end position can be recalculated upon every horizontal input event
@@ -65,11 +67,11 @@ class UI {
                     cellElement.style.gridRowEnd = `${rowIndex + 2}`;
                     cellElement.style.gridColumnStart = `${columnIndex + 1}`;
                     cellElement.style.gridColumnEnd = `${columnIndex + 2}`;
-                    shapeContainer.appendChild(cellElement);
+                    this.shapeContainer.appendChild(cellElement);
                 }
             })
         })
-        
+
         // shapeContainer.classList.add("outline");
         let leftShiftMultiplier = 0;
 
@@ -94,7 +96,7 @@ class UI {
 
         const leftOrigin = (this.cellSize * leftShiftMultiplier) * -1
 
-        shapeContainer.style.left = `${leftOrigin}px`;
+        this.shapeContainer.style.left = `${leftOrigin}px`;
 
         /* 
             Make the middle of the grid the starting point, then set the max and min
@@ -102,57 +104,71 @@ class UI {
             use this line: (this.columns / 2 * this.cellSize)
         */
 
-        let horizontalMove = 0; 
-
-        shapeContainer.classList.add('outline');
-        this.grid.appendChild(shapeContainer); 
+        // this.shapeContainer.classList.add('outline'); 
+        this.grid.appendChild(this.shapeContainer); 
 
         const playAnimation = true; 
 
         let verticalPosition = 0;
 
+        document.addEventListener('keydown', this)
+
         if(playAnimation) {
+
             setTimeout(() => {
                     const animation = setInterval(() =>  {
                         verticalPosition += 1; 
-                        shapeContainer.style.top = `${verticalPosition * this.cellSize}px`
-    
+                        this.shapeContainer.style.top = `${verticalPosition * this.cellSize}px`; 
+
                         if(verticalPosition === endAnimationPosition) {
-                            /* this point I can call gameBoard.placeTetromino */
-                            const horizontalPosition = (leftShiftMultiplier * -1 ) + horizontalMove
-                            console.log(`horizontal: ${horizontalPosition}\nvertical: ${verticalPosition}`)
-                            /* remove the eventListener from the shapeContainer*/
+                            /* this point I can call gameBoard.placeTetromino */  
+                            const horizontalPosition = (leftShiftMultiplier * -1 ) + this.horizontalMove
+                            console.log(`horizontal: ${horizontalPosition} \nvertical: ${verticalPosition}`)
+                            /* remove the eventListener from the shapeContainer*/  
+                            document.removeEventListener('keydown', this)
+                            this.fallingTetro = false;
+
+                            
+                            // this.drawTetromino()
+                            
+                            /*
+                            
+                                recursion works amazingly, maybe that is the way forward? Only making the new call after a setTimeOut and other conditions satisfied? 
+                            */
                             clearInterval(animation)
                         }
                     }, 500)
             }, 2000)
         }
-
-        document.addEventListener('keydown' , (e) => {
-            if (!e.repeat) {
-                if(e.key === "ArrowLeft"){
-                    console.log('left arrow clicked')
-                    if(horizontalMove > 0) {
-                        horizontalMove -= 1; 
-                    }
-                } else if(e.key === "ArrowRight") {
-                    console.log('right arrow clicked')
-                    if(horizontalMove < this.columns - 1) {
-                        /* this -, currently hardcoded for the straight tetro will be account for the
-                        width of the shape
-                        */
-                       horizontalMove += 1
-                    }
-                }
-            }
-            shapeContainer.style.left = `${leftOrigin + (horizontalMove * this.cellSize) }px`;
-
-        })
-
     }
 
     tetrominoVerticalAnimation(tetrominoContainer) {
         return
+    }
+
+    handleEvent(e) {
+        if(e.type === 'keydown'){
+            if (!e.repeat) {
+                if(e.key === "ArrowLeft"){
+                    console.log('left arrow clicked')
+                    if(this.horizontalMove > 0) {
+                        this.horizontalMove -= 1; 
+                    }
+                } else if(e.key === "ArrowRight") {
+                    console.log('right arrow clicked')
+                    if(this.horizontalMove < this.columns - 1) {
+                       this.horizontalMove += 1
+                    }
+                }
+                this.shapeContainer.style.left = `${-24 + (this.horizontalMove * this.cellSize) }px`;
+                console.log(`horizontal move: ${this.horizontalMove}`)
+            }
+        }
+    }
+
+    generateTetro() {
+        const color = Math.floor(Math.random() * 6) + 1;
+        return new IotaTetromino(color)
     }
 
     getCellColor(colorId) {
@@ -164,7 +180,7 @@ class UI {
             'bg-blue-600', 
             'bg-violet-700'
         ]
-        return colors[colorId-1]
+        return colors[colorId- 1]
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 import UI from "./classes/userInterface.js";
 import "./styles.css";
+import IotaTetromino from "./classes/tetromino/iotaTetromino.js";
 
-const test = new UI();
+const ui = new UI();
+const testIota = new IotaTetromino(2); 
+
+let iterationCount = 0
+
+
+
+/* 
+
+test.draw() => draws the tetro
+test.animateDown() => returns true or false
+
+let falling = test.animateDown()
+
+    test.draw();
+        test.animate();
+
+
+    async run() {
+        while(true) {
+            let tetro = new tetro(n);
+            await test.draw();
+            if(no lose) {
+                generateNewTetro()
+                await test.draw(newTetro)
+            }
+        }
+    }
+        
+    let test.draw return a promise 
+}
+
+*/
