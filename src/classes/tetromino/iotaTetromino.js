0f5cb43c77544d454d10c1ab79ee8d9d781ca347
luya <91703950+lmaqungo@users.noreply.github.com>
2026-04-30 11:47:13 +0200

    feat: rotation feat updates the class structure
    constructor will generate a random frame; updateShape will use the
    frame field to generate the shape field. The I Tetromino will serve as
    the blueprint for all the other tetrominoes.

--- src/classes/tetromino/iotaTetromino.js
+++ src/classes/tetromino/iotaTetromino.js
@@ -2,16 +2,58 @@ import Tetromino from "./tetromino.js";
 
 class IotaTetromino extends Tetromino {
     shape; 
+    frame; 
+    colorId;
+    width;
 
-    constructor(id) {
+    constructor(id, frame=1) {
         super(); 
-        this.shape = [
-            [0, id, 0, 0], 
-            [0, id, 0, 0], 
-            [0, id, 0, 0], 
-            [0, id, 0, 0] 
-        ]; 
+        this.colorId = id;
         
+        this.frame = this.getRandomFrame(frame); 
+        this.updateShape()
+    }
+
+    updateShape() {
+        const id = this.colorId
+        switch(this.frame) {
+            case 1: 
+                this.shape = [
+                    [0, id, 0, 0], 
+                    [0, id, 0, 0], 
+                    [0, id, 0, 0], 
+                    [0, id, 0, 0]
+                ]; 
+                this.width = 1;
+                break;
+            case 2: 
+                this.shape = [
+                    [0, 0, 0, 0], 
+                    [0, 0, 0, 0], 
+                    [0, 0, 0, 0], 
+                    [id, id, id, id]
+                ]; 
+                this.width = 4;
+                break;
+            case 3: 
+                this.shape = [
+                    [0, 0, id, 0], 
+                    [0, 0, id, 0], 
+                    [0, 0, id, 0], 
+                    [0, 0, id, 0]
+                ]; 
+                this.width = 1;
+                break;
+            case 4: 
+                this.shape = [
+                    [0, 0, 0, 0], 
+                    [0, 0, 0, 0], 
+                    [0, 0, 0, 0], 
+                    [id, id, id, id]
+                ]; 
+                this.width = 1;
+                break;
+        }
     }
 
     get shape() {
@@ -20,6 +62,11 @@ class IotaTetromino extends Tetromino {
 
     set shape(input) {
         this.shape = input
+    }
+
+    rotate() {
+        this.frame = this.incrementFrame(this.frame); 
+        this.updateShape()
     }
 
     toString() {
