5e50c9c0b32badb009f87c871f1397af99ca809c
luya <91703950+lmaqungo@users.noreply.github.com>
2026-04-30 11:44:35 +0200

    feat: getRandomFrame and incrementFrame fns
    1. For rotation to work, added function that increments the rotation
    frame.
    2. When tetromino is instantiated, a random frame will be generated

--- src/classes/tetromino/tetromino.js
+++ src/classes/tetromino/tetromino.js
@@ -1,3 +1,4 @@
+
 class Tetromino {
 
     constructor(){
@@ -8,6 +9,24 @@ class Tetromino {
 
     get shape(){
         throw new Error('get shape() is an abstract method that must be implemented by subclass')
+    }
+
+    getRandomFrame(frame=null) {
+        if(frame){
+            return frame
+        } else {
+            return Math.floor(Math.random() * 4) + 1
+        }
+    }
+
+    incrementFrame(startFrame) {
+        let frame = startFrame; 
+        if(frame === 4) {
+            frame = 1
+        } else {
+            frame += 1
+        }
+        return frame;
     }
 
 }
