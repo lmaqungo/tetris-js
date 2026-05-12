254452cde61d9ffb2cef6d1083e30f0b1ed6fa65
luya <91703950+lmaqungo@users.noreply.github.com>
2026-05-12 16:04:42 +0200

    feat: updated lambda class
    added updateShape and rotate fns

--- src/classes/tetromino/lambdaTetromino.js
+++ src/classes/tetromino/lambdaTetromino.js
@@ -2,44 +2,71 @@ import Tetromino from "./tetromino.js";
 
 class LambdaTetromino extends Tetromino {
     shape; 
+    frame; 
+    colorId; 
+    width; 
     
-
-    constructor(id) {
+    constructor(id, frame=1) {
         super(); 
-        this.shape = [
-            [id, 0, 0], 
-            [id, 0, 0], 
-            [id, id, 0]
-        ]; 
+        this.colorId = id; 
+
+        this.frame = this.getRandomFrame(frame); 
+        this.updateShape()
        
     }
 
-    get shape(){
-        return this.shape
+    updateShape(){
+        const id = this.colorId; 
+        switch(this.frame) {
+            case 1:
+                this.shape = [
+                    [id, 0, 0], 
+                    [id, 0, 0], 
+                    [id, id, 0]
+                ];
+                this.width = 2; 
+                break;
+            case 2:
+                this.shape = [
+                    [0, 0, 0], 
+                    [id, id, id], 
+                    [id, 0, 0]
+                ];
+                this.width = 3; 
+                break;
+            case 3:
+                this.shape = [
+                    [0, id, id], 
+                    [0, 0, id], 
+                    [0, 0, id]
+                ];
+                this.width = 2; 
+                break;
+            case 4:
+                this.shape = [
+                    [0, 0, 0], 
+                    [0, 0, id], 
+                    [id, id, id]
+                ];
+                this.width = 3; 
+                break;
+        }
     }
 
-    set shape(input) {
-        this.shape = input
+    rotate() {
+        this.frame = this.incrementFrame(this.frame); 
+        this.updateShape();
     }
 
-    toString() {
-        let shapeString = ''; 
-        this.shape.forEach((row, rowIndex) => {
-            let rowString = ''; 
-            row.forEach((cell) => {
-                if(cell === 0){
-                    rowString+= ' '
-                } else{
-                    rowString += cell
-                }
-            })
-            shapeString+= rowString
-            if(rowIndex < this.shape.length - 1) {
-                shapeString+= '\n'
-            }
-        })
-        return shapeString
-    }
+}
+
+for(let i = 1; i < 5; i++){
+    const lambda = new LambdaTetromino(2, i)
+    console.log(lambda.toString()); 
+    
+    lambda.getBottomCells().forEach((coord) => {
+        console.log(`row: ${coord.row}, col: ${coord.col}`)
+    })
 }
 
 export default LambdaTetromino
